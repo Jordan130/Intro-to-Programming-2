80252ecf38531a4f5c98c8be9eb0e2f7d07d02a1
Jordan Rios <145077985+Jordan130@users.noreply.github.com>
2023-12-30 23:48:12 -0500

    Add files via upload

---
+++ Lab02/StudentTests[1].java
@@ -0,0 +1,268 @@
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.FixMethodOrder;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+/*
+ * You must include the Junit4 library to use this test unit.
+ *
+ * To add it, select the project and then choose Properties... from the File menu.
+ * Click on "Java Build Path" in the panel at the left.
+ * Select Libraries from the list at the top.
+ * Select Add Library... from the right side of the panel.
+ * Select JUnit and click on Next.
+ * Change the version to JUnit 4 and click on Finish.
+ * Click on OK in the properties panel.
+ * 
+ * You can then run these tests using "Run As JUnit Test" instead of "Runs As Java Application".
+ */
+
+
+/**
+ * JUnit tests for Statistician.
+ *
+ * @author Beth Katz, David Hutchens, Chad Hogg
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class StudentTests {
+	
+	/** The maximum allowable difference when comparing doubles. */
+	private static double EPSILON = 0.00001;
+	
+	/**
+	 * Creates a Statistician and checks whether it was constructed properly.
+	 */
+	@Test
+	public void test01Constructor() {
+		Statistician statistician = new Statistician();
+
+		assertEquals("New Statistician had wrong sum.", 0.0, statistician.getSum(), EPSILON);
+		assertEquals("New Statistician had wrong length.", 0, statistician.getLength());
+		assertTrue("New Statistician's mean should be Double.NaN.", Double.isNaN(statistician.getMean()));
+		assertTrue("New Statistician's largest value should be Double.NaN.", Double.isNaN(statistician.getLargest()));
+		assertTrue("New Statistician's smallest value should be Double.NaN.", Double.isNaN(statistician.getSmallest()));
+	}
+
+	/**
+	 * Checks that inserting into a Statistician increments the length.
+	 */
+	@Test
+	public void test02IncrementLength() {
+		Statistician statistician = new Statistician();
+		statistician.insert(42);
+		assertEquals("Statistician's length is wrong after one insertion.", 1, statistician.getLength());
+		statistician.insert(12);
+		statistician.insert(17);
+		assertEquals("Statistician's length is wrong after three inserations.", 3, statistician.getLength());
+	}
+
+
+	/**
+	 * Tests that reset returns everything to original values.
+	 */
+	@Test
+	public void test03Reset() {
+		Statistician statistician = new Statistician();
+
+		statistician.insert(42);
+		statistician.insert(17);
+		statistician.insert(-5.5);
+		statistician.insert(0);
+
+		assertEquals("Statistician's sum is wrong after inserting 42, 17, -5.5, and 0.", 53.5, statistician.getSum(), EPSILON);
+		assertEquals("Statistician's length is wrong after inserting 42, 17, -5.5, and 0.", 4, statistician.getLength());
+		assertEquals("Statistician's mean is wrong after inserting 42, 17, -5.5, and 0.", 13.375, statistician.getMean(), EPSILON);
+		assertEquals("Statistician's largest is wrong after inserting 42, 17, -5.5, and 0.", 42, statistician.getLargest(), EPSILON);
+		assertEquals("Statistician's smallest is wrong after inserting 42, 17, -5.5, and 0.", -5.5, statistician.getSmallest(), EPSILON);
+
+		statistician.reset();	
+
+		assertEquals("Statistician's sum is wrong after resetting.", 0.0, statistician.getSum(), EPSILON);
+		assertEquals("Statistician's length is wrong after resetting.", 0, statistician.getLength());
+		assertTrue("Statistician's mean should be Double.NaN after resetting.", Double.isNaN(statistician.getMean()));
+		assertTrue("Statistician's largest should be Double.NaN after resetting.", Double.isNaN(statistician.getLargest()));
+		assertTrue("Statistician's smallest should be Double.NaN after resetting.", Double.isNaN(statistician.getSmallest()));
+	}
+
+	/**
+	 * Tests behavior of Statistician when all values are negative.
+	 */
+	@Test
+	public void test04AllNegative( ) {
+		Statistician statistician = new Statistician();
+
+		statistician.insert(-42);
+		statistician.insert(-10);
+		statistician.insert(-8.15);
+
+		assertEquals("Statistician's sum is wrong after inserting -42, -10, and -8.15.", -60.15, statistician.getSum(), EPSILON);
+		assertEquals("Statistician's length is wrong after inserting -42, -10, and -8.15.", 3, statistician.getLength());
+		assertEquals("Statistician's mean is wrong after inserting -42, -10, and -8.15.", -20.05, statistician.getMean(), EPSILON);
+		assertEquals("Statistician's largest is wrong after inserting -42, -10, and -8.15.", -8.15, statistician.getLargest(), EPSILON);
+		assertEquals("Statistician's smallest is wrong after inserting -42, -10, and -81.5.", -42.0, statistician.getSmallest(), EPSILON);
+	}
+
+	/**
+	 * Tests behavior of Statistician when smallest value is zero.
+	 */
+	@Test
+	public void test05SmallestZero( ) {
+		Statistician statistician = new Statistician();
+
+		statistician.insert(1);
+		statistician.insert(0);
+		statistician.insert(2);
+
+		assertEquals("Statistician's smallest is wrong after inserting 1, 0, and 2.", 0.0, statistician.getSmallest(), EPSILON);
+	}
+
+	/**
+	 * Creates Statisticians with a lot of values and sees
+	 *   if that causes problems. But it doesn't try to
+	 *   overflow it.
+	 */
+	@Test
+	public void test06CompareALot() {
+		Statistician statistician = new Statistician();
+		for (int counter = 1; counter <= 10000; counter++) {
+			statistician.insert(counter);
+		}
+
+		assertEquals("Statistician's mean is wrong after adding the numbers 1 through 10000 (inclusive).", 5000.5, statistician.getMean(), EPSILON);
+		assertEquals("Statistician's length is wrong after adding the numbers 1 through 10000 (inclusive).", 10000, statistician.getLength());
+	}
+
+	/**
+	 * Creates two statisticians, adds them, and checks answers.
+	 */
+	@Test
+	public void test07Union() {
+		Statistician first = new Statistician();
+		Statistician second = new Statistician();
+		Statistician union = first.union(second);
+
+		assertEquals("Statistician's length was wrong after unioning it with another.", 0, first.getLength());
+		assertEquals("Statistician's length was wrong after unioning it with another.", 0, second.getLength());
+		assertEquals("Union of two empty Statisticians had incorrect length.", 0, union.getLength());
+		assertTrue("Union of two empty Statisticians should have had Double.NaN as largest.", Double.isNaN(union.getLargest()));
+		assertTrue("Union of two empty Statisticians should have had Double.NaN as smallest.", Double.isNaN(union.getSmallest()));
+
+		for(int counter = 1; counter <= 3; counter++) {
+			first.insert(counter);
+		}
+		for(int counter = 5; counter <= 7; counter++) {
+			second.insert(counter);
+		}
+
+		union = first.union(second);
+
+		assertEquals("Union of <1, 2, 3> with <5, 6, 7> had wrong length.", 6, union.getLength());
+		assertEquals("Union of <1, 2, 3> with <5, 6, 7> had wrong sum.", first.getSum() + second.getSum(), union.getSum(), EPSILON);
+		assertEquals("Union of <1, 2, 3> with <5, 6, 7> had wrong largest.", 7.0, union.getLargest(), EPSILON);
+		assertEquals("Union of <1, 2, 3> with <5, 6, 7> had wrong smallest.", 1.0, union.getSmallest(), EPSILON);
+	}
+
+	/**
+	 * Creates two empty statisticians, adds them, and checks answers.
+	 */
+	@Test
+	public void test08AddEmpty() {
+
+		Statistician first = new Statistician();
+		Statistician second = new Statistician();
+		first.add(second);
+
+		assertEquals("New Statistician that had another new Statistician added had wrong length.", 0, first.getLength());
+		assertEquals("New Statistician that was added to another Statistician had wrong length.", 0, second.getLength());
+		assertTrue("New Statistician that had another new Statistician added should have had Double.NaN for largest.", Double.isNaN(first.getLargest()));
+		assertTrue("New Statistician that had another new Statistician added should have had Double.NaN for smallest.", Double.isNaN(first.getSmallest()));
+	}
+
+
+	/**
+	 * Creates two Statisticians, adds them and unions them, and checks largest and smallest.
+	 */
+	@Test
+	public void test09UnionSmall() {
+
+		Statistician first = new Statistician();
+		Statistician second = new Statistician();
+		Statistician third = new Statistician();
+		double value = 42.0;
+
+		first.insert(value);  // first contains one
+
+		first.add(third);   // add empty to first
+		assertEquals("Smallest was wrong after adding empty Statistician to <42.0> one.", value, first.getSmallest(), EPSILON);
+		assertEquals("Largest was wrong after adding empty Statistician to <42.0> one.", value, first.getLargest(), EPSILON);
+
+		second.add(first);   // add first to empty
+		assertEquals("Smallest was wrong after adding <42.0> Statistician to empty one.", value, second.getSmallest(), EPSILON);
+		assertEquals("Largest was wrong after adding <42.0> Statistician to empty one.", value, second.getLargest(), EPSILON);
+
+		second.insert(value * 2 + 10);
+		third = first.union(second);  // union the two
+
+		assertEquals("Length was wrong in union of <42.0> Statistician with <42.0, 94.0> Statistician.", 3, third.getLength());
+		assertEquals("Sum was wrong in union of <42.0> Statistician with <42.0, 94.0> Statistician.", second.getSum() + first.getSum(), third.getSum(), EPSILON);
+		assertEquals("Largest was wrong in union of <42.0> Statistician with <42.0, 94.0> Statistician.", 94.0, third.getLargest(), EPSILON);
+		assertEquals("Smallest was wrong in union of <42.0> Statistician with <42.0, 94.0> Statistician.", 42.0, third.getSmallest(), EPSILON);
+	}
+
+	/**
+	 * Creates two statisticians, adds them, and checks largest and smallest.
+	 */
+	@Test
+	public void test10AddExtremes() {
+
+		Statistician first = new Statistician();
+		Statistician second = new Statistician();
+		first.insert(17.17);
+		first.insert(78.0);
+		second.insert(4.0);
+		second.insert(100.0);
+
+		second.add(first);
+		assertEquals("Smallest was wrong after adding <17.17, 78.0> Statistician to <4.0, 100.0> one.", 4.0, second.getSmallest(), EPSILON);
+		assertEquals("Largest was wrong after adding <17.17, 78.0> Statistician to <4.0, 100.0> one.", 100.0, second.getLargest(), EPSILON);
+	}
+
+	/**
+	 * Looks for bug in passing a null pointer to union.
+	 */
+	@Test
+	public void test11UnionException() {
+		try {
+			Statistician first = new Statistician();
+			Statistician second = null;
+			first.union(second);
+			fail("Trying to union a Statistician with null should have resulted in a NullPointerException.");
+		}
+		catch(NullPointerException exception) {
+			// expected
+		}
+	}
+
+	/**
+	 * Looks for bug in passing a null pointer to add.
+	 */
+	@Test
+	public void test12AddException() {
+		try {
+			Statistician first = new Statistician();
+			Statistician second = null;
+			first.add(second);
+			fail("Trying to add null to a Statistician should have resulted in a NullPointerException.");
+		}
+		catch(NullPointerException exception) {
+			// expected
+		}
+	}
+
+}
